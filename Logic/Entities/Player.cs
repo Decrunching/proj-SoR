4db8acd69fb9df22c5749e3e262b2ca36570ad1c
Cyonica <monkey_lady@hotmail.com>
2024-08-15 17:13:21 +0100

    Added updated sprites and animations, and added skin support

--- Logic/Entities/Player.cs
+++ Logic/Entities/Player.cs
@@ -12,55 +12,70 @@ namespace SoR.Logic.Entities
         protected SkeletonJson json;
         protected SkeletonData skeletonData;
         protected Skeleton skeleton;
+        protected Skin skin;
         protected AnimationState animState;
         protected AnimationStateData animStateData;
 
         public Player(GraphicsDevice GraphicsDevice)
         {
             // Load texture atlas and attachment loader
-            atlas = new Atlas("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\skeleton.atlas", new XnaTextureLoader(GraphicsDevice));
+            atlas = new Atlas("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\haltija.atlas", new XnaTextureLoader(GraphicsDevice));
+            //atlas = new Atlas("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\skeleton.atlas", new XnaTextureLoader(GraphicsDevice));
             //atlas = new Atlas("D:\\GitHub projects\\Proj-SoR\\Content\\Entities\\Player\\skeleton.atlas", new XnaTextureLoader(game.GraphicsDevice));
             atlasAttachmentLoader = new AtlasAttachmentLoader(atlas);
             json = new SkeletonJson(atlasAttachmentLoader);
 
             // Initialise skeleton json to be loaded at 0.5x scale
             skeletonData = json.ReadSkeletonData("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\skeleton.json");
+            //skeletonData = json.ReadSkeletonData("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\skeleton.json");
             //skeletonData = json.ReadSkeletonData("D:\\GitHub projects\\Proj-SoR\\Content\\Entities\\Player\\skeleton.json");
             skeleton = new Skeleton(skeletonData);
+
+            //Add a new skin
+            SetSkin("1");
 
             // Setup animation
             animStateData = new AnimationStateData(skeleton.Data);
             animState = new AnimationState(animStateData);
 
             // Set the "fidle" animation on track 1 and leave it looping forever
-            animState.SetAnimation(0, "fdownidle", true);
+            skeleton.SetSkin(skin);
+            skeleton.SetSlotsToSetupPose();
+            animState.Apply(skeleton);
+            animState.SetAnimation(0, "idle", true);
 
             // 0.2 seconds of mixing time between animation transitions
-            animStateData.DefaultMix = 0.2f;
+            animStateData.DefaultMix = 0.6f;
+        }
+
+        public void SetSkin(string skinName)
+        {
+            skin = skeletonData.FindSkin(skinName);
+            if (skin == null) throw new System.ArgumentException("Can't find skin: " + skinName);
         }
 
         public void SetAnimState(KeyboardState keyState, KeyboardState lastKeyState)
         {
             if (keyState.IsKeyDown(Keys.Up) & !lastKeyState.IsKeyDown(Keys.Up))
             {
-                animState.AddAnimation(0, "fup", true, 0);
+                animState.AddAnimation(0, "runup", true, 0);
             }
 
             if (keyState.IsKeyDown(Keys.Down) & !lastKeyState.IsKeyDown(Keys.Down))
             {
-                animState.AddAnimation(0, "fdown", true, 0);
+                animState.AddAnimation(0, "rundown", true, 0);
             }
 
             if (keyState.IsKeyDown(Keys.Left) & !lastKeyState.IsKeyDown(Keys.Left))
             {
-                animState.AddAnimation(0, "fside", true, 0);
-                skeleton.ScaleX = -1;
+                animState.AddAnimation(0, "runleft", true, 0);
+                //skeleton.ScaleX = -1;
             }
 
             if (keyState.IsKeyDown(Keys.Right) & !lastKeyState.IsKeyDown(Keys.Right))
             {
-                animState.AddAnimation(0, "fside", true, 0);
-                skeleton.ScaleX = 1;
+                animState.AddAnimation(0, "runright", true, 0);
+                //skeleton.ScaleX = 1;
             }
         }
 
