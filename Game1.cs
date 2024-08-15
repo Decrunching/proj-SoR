69afd2146857eed0998981f3a6a669a434eda7c4
Cyonica <monkey_lady@hotmail.com>
2024-08-15 14:28:00 +0100

    Got input working. Was not passing position between classes correctly,

--- Game1.cs
+++ Game1.cs
@@ -1,7 +1,9 @@
-﻿using Spine;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SoR.Logic.Entities;
+using Spine;
+
 
 namespace SoR
 {
@@ -19,13 +21,9 @@ namespace SoR
         private bool keyPressed;
 
         protected SkeletonRenderer skeletonRenderer;
-        protected AtlasAttachmentLoader atlasAttachmentLoader;
-        protected Atlas atlas;
-        protected SkeletonJson json;
-        protected SkeletonData skeletonData;
-        protected Skeleton skeleton;
-        protected AnimationState animState;
-        protected AnimationStateData animStateData;
+
+        private Game1 game;
+        private Player player;
 
         public Game1()
         {
@@ -41,6 +39,7 @@ namespace SoR
             keyPressed = false;
             speed = 300f;
             deadZone = 4096;
+            game = this;
         }
 
         protected override void Initialize()
@@ -54,30 +53,12 @@ namespace SoR
 
         protected override void LoadContent()
         {
-            // TODO: use this.Content to load your game content here// Initialise skeleton renderer with premultiplied alpha
+            // TODO: use this.Content to load your game content here
+            // Initialise skeleton renderer with premultiplied alpha
             skeletonRenderer = new SkeletonRenderer(GraphicsDevice);
             skeletonRenderer.PremultipliedAlpha = true;
 
-            // Load texture atlas and attachment loader
-            atlas = new Atlas("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\skeleton.atlas", new XnaTextureLoader(GraphicsDevice));
-            //atlas = new Atlas("D:\\GitHub projects\\Proj-SoR\\Content\\Entities\\Player\\skeleton.atlas", new XnaTextureLoader(game.GraphicsDevice));
-            atlasAttachmentLoader = new AtlasAttachmentLoader(atlas);
-            json = new SkeletonJson(atlasAttachmentLoader);
-
-            // Initialise skeleton json to be loaded at 0.5x scale
-            skeletonData = json.ReadSkeletonData("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\skeleton.json");
-            //skeletonData = json.ReadSkeletonData("D:\\GitHub projects\\Proj-SoR\\Content\\Entities\\Player\\skeleton.json");
-            skeleton = new Skeleton(skeletonData);
-
-            // Setup animation
-            animStateData = new AnimationStateData(skeleton.Data);
-            animState = new AnimationState(animStateData);
-
-            // Set the "fidle" animation on track 1 and leave it looping forever
-            animState.SetAnimation(0, "fdownidle", true);
-
-            // 0.2 seconds of mixing time between animation transitions
-            animStateData.DefaultMix = 0.2f;
+            player = new Player(GraphicsDevice);
         }
 
         protected override void Update(GameTime gameTime)
@@ -93,10 +74,7 @@ namespace SoR
             {
                 //keyPressed = true;
                 position.Y -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                if (!lastKeyState.IsKeyDown(Keys.Up))
-                {
-                    animState.AddAnimation(0, "fup", true, 0);
-                }
+                player.SetAnimState(lastKeyState);
                 //lastKey = "up";
             }
 
@@ -104,10 +82,7 @@ namespace SoR
             {
                 //keyPressed = true;
                 position.Y += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                if (!lastKeyState.IsKeyDown(Keys.Down))
-                {
-                    animState.AddAnimation(0, "fdown", true, 0);
-                }
+                player.SetAnimState(lastKeyState);
                 //lastKey = "down";
             }
 
@@ -115,11 +90,7 @@ namespace SoR
             {
                 //keyPressed = true;
                 position.X -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                if (!lastKeyState.IsKeyDown(Keys.Left))
-                {
-                    animState.AddAnimation(0, "fside", true, 0);
-                    skeleton.ScaleX = -1;
-                }
+                player.SetAnimState(lastKeyState);
                 //lastKey = "left";
             }
 
@@ -127,11 +98,7 @@ namespace SoR
             {
                 //keyPressed = true;
                 position.X += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                if (!lastKeyState.IsKeyDown(Keys.Right))
-                {
-                    animState.AddAnimation(0, "fside", true, 0);
-                    skeleton.ScaleX = 1;
-                }
+                player.SetAnimState(lastKeyState);
                 //lastKey = "right";
             }
 
@@ -162,15 +129,7 @@ namespace SoR
 
             lastKeyState = keyState;
 
-            // Update the animation state and apply animations to skeletons
-            skeleton.X = position.X;
-            skeleton.Y = position.Y;
-            animState.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
-            skeleton.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
-            animState.Apply(skeleton);
-
-            // Update skeletal transformations
-            skeleton.UpdateWorldTransform(Skeleton.Physics.Update);
+            player.UpdateSkeletalAnimations(gameTime, position);
 
             base.Update(gameTime);
         }
@@ -188,7 +147,7 @@ namespace SoR
 
             // Draw skeletons
             skeletonRenderer.Begin();
-            skeletonRenderer.Draw(skeleton);
+            skeletonRenderer.Draw(player.GetSkeleton());
             skeletonRenderer.End();
 
             base.Draw(gameTime);
--- Logic/Entities/Player.cs
+++ Logic/Entities/Player.cs
@@ -1,13 +1,85 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Spine;
 
 namespace SoR.Logic.Entities
 {
-    internal class Player : Game1
+    internal class Player
     {
+        protected AtlasAttachmentLoader atlasAttachmentLoader;
+        protected Atlas atlas;
+        protected SkeletonJson json;
+        protected SkeletonData skeletonData;
+        protected Skeleton skeleton;
+        protected AnimationState animState;
+        protected AnimationStateData animStateData;
+
         public Player(GraphicsDevice GraphicsDevice)
         {
-            
+            // Load texture atlas and attachment loader
+            atlas = new Atlas("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\skeleton.atlas", new XnaTextureLoader(GraphicsDevice));
+            //atlas = new Atlas("D:\\GitHub projects\\Proj-SoR\\Content\\Entities\\Player\\skeleton.atlas", new XnaTextureLoader(game.GraphicsDevice));
+            atlasAttachmentLoader = new AtlasAttachmentLoader(atlas);
+            json = new SkeletonJson(atlasAttachmentLoader);
+
+            // Initialise skeleton json to be loaded at 0.5x scale
+            skeletonData = json.ReadSkeletonData("F:\\MonoGame\\SoR\\SoR\\Content\\Entities\\Player\\skeleton.json");
+            //skeletonData = json.ReadSkeletonData("D:\\GitHub projects\\Proj-SoR\\Content\\Entities\\Player\\skeleton.json");
+            skeleton = new Skeleton(skeletonData);
+
+            // Setup animation
+            animStateData = new AnimationStateData(skeleton.Data);
+            animState = new AnimationState(animStateData);
+
+            // Set the "fidle" animation on track 1 and leave it looping forever
+            animState.SetAnimation(0, "fdownidle", true);
+
+            // 0.2 seconds of mixing time between animation transitions
+            animStateData.DefaultMix = 0.2f;
+        }
+
+        public void SetAnimState(KeyboardState lastKeyState)
+        {
+            if (!lastKeyState.IsKeyDown(Keys.Up))
+            {
+                animState.AddAnimation(0, "fup", true, 0);
+            }
+
+            if (!lastKeyState.IsKeyDown(Keys.Down))
+            {
+                animState.AddAnimation(0, "fdown", true, 0);
+            }
+
+            if (!lastKeyState.IsKeyDown(Keys.Left))
+            {
+                animState.AddAnimation(0, "fside", true, 0);
+                skeleton.ScaleX = -1;
+            }
+
+            if (!lastKeyState.IsKeyDown(Keys.Right))
+            {
+                animState.AddAnimation(0, "fside", true, 0);
+                skeleton.ScaleX = 1;
+            }
+        }
+
+        public void UpdateSkeletalAnimations(GameTime gameTime, Vector2 position)
+        {
+            // Update the animation state and apply animations to skeletons
+            skeleton.X = position.X;
+            skeleton.Y = position.Y;
+            animState.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+            skeleton.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+            animState.Apply(skeleton);
+
+            // Update skeletal transformations
+            skeleton.UpdateWorldTransform(Skeleton.Physics.Update);
+        }
+
+        public Skeleton GetSkeleton()
+        {
+            return skeleton;
         }
     }
 }
